85559e992215e8fc5906a1ddaf99565b6a9f46c6
Abdullah Ibne Hanif Arean <abdullaharean2613@gmail.com>
2023-02-04 18:18:35 +0600

    Project 4 Made Modular still bugs as filedata not transfered

--- Project4_HTTPSocketFileServer/src/FileClient.java
+++ Project4_HTTPSocketFileServer/src/FileClient.java
@@ -3,98 +3,148 @@ import java.net.*;
 import java.util.Scanner;
 
 public class FileClient {
+    static Socket socket = null;
     public static void main(String[] args) throws IOException {
         // Set the server's IP address and the file name you want to download
         String serverIP = "localhost";
-        System.out.println("What do you want to do? 1. Send\n2.Receive");
-        String userinp = (new Scanner(System.in)).nextLine();
-        Socket socket = null;
         // Create a socket to connect to the server
         socket = new Socket(serverIP, 8280);
-        if (userinp.equals("2")) {
-            System.out.println("Give the file name you want to download! (will be downloaded in out folder)");
-            String workingDirectory = System.getProperty("user.dir");
-            String userinp1 = (new Scanner(System.in)).nextLine();
-            String saveFilePath = workingDirectory + File.separator + "serverstorage"+File.separator+userinp1+".txt";
-            // Open an output stream to send the GET request
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            // Send the GET request
-            out.println("GET /" + saveFilePath + " HTTP/1.0");
-            out.println();
-            out.flush();
+        while (true) {
+            System.out.println("What do you want to do? 1. Receive (Download) \n2.Send (Upload)\n3.Exit");
+            String userinp = (new Scanner(System.in)).nextLine();
+            boolean getout=true;
+            switch (userinp) {
+                case "1" -> {
+                    while (getout) {
+                        System.out.println("""
+                                Give the file name you want to download! (will be downloaded in out folder)
+                                1. file1\s
+                                2. file2\s
+                                3.file3\s
+                                4. exit""");
+                        switch ((new Scanner(System.in)).nextLine()) {
+                            case "1" -> downloadfiles("file1");
+                            case "2" -> downloadfiles("file2");
+                            case "3" -> downloadfiles("file3");
+                            case "4" -> getout = false;
+                            default -> System.out.println("Invalid Input");
+                        }
 
-            // Open an input stream to read the server's response
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    }
+                }
+                case "2" -> {
+                    while (getout) {
+                        System.out.println("""
+                                Give the file name you want to upload! (will be uploaded in serverstorage folder)
+                                1. file1\s
+                                2. file2\s
+                                3.file3\s
+                                4. exit""");
 
-            // Read the server's response
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = in.readLine()) != null) {
-                response.append(line).append("\n");
-            }
+                        switch ((new Scanner(System.in)).nextLine()) {
+                            case "1" -> uploadfiles("file1");
+                            case "2" -> uploadfiles("file2");
+                            case "3" -> uploadfiles("file3");
+                            case "4" -> getout = false;
+                            default -> System.out.println("Invalid Input! Try Again");
+                        }
 
-            // Check if the server's response is a "200 OK"
-            if (response.toString().startsWith("HTTP/1.0 200 OK")) {
-                // Extract the file data from the response
-                int fileStart = response.indexOf("\r\n\r\n") + 4;
-                int fileEnd = response.length() - 1;
-                String fileData = response.substring(fileStart, fileEnd);
-                saveFilePath = workingDirectory + File.separator + "clientstorage"+File.separator+"Received"+System.currentTimeMillis()+"_"+userinp1+".txt";
-
-                // Open a file output stream to save the file
-                FileOutputStream fileOutputStream = new FileOutputStream(saveFilePath);
-
-                // Write the file data to the file output stream
-                fileOutputStream.write(fileData.getBytes());
-
-                // Close the file output stream
-                fileOutputStream.close();
-                System.out.println("File saved to disk.");
-            } else {
-                System.out.println("Error: " + response);
+                    }
+                }
+                case "3" -> {
+                    // Close the socket
+                    socket.close();
+                    return;
+                }
+                default -> System.out.println("Invalid Input");
             }
         }
-        else if(userinp.equals("1")){
-            // Open an output stream to send the request
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            // Open a file input stream to read the file
-            String workingDirectory = System.getProperty("user.dir");
-            String userinp1 = (new Scanner(System.in)).nextLine();
-            String saveFilePath = workingDirectory + File.separator + "src"+File.separator+userinp1+".txt";
-            FileInputStream fileInputStream = new FileInputStream(saveFilePath);
 
-            // Read the file into a byte array
-            byte[] fileData = new byte[fileInputStream.available()];
-            fileInputStream.read(fileData);
+    }
 
-            // Close the file input stream
-            fileInputStream.close();
+    private static void uploadfiles(String filename) throws IOException {
+        // Open an output stream to send the request
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            // Send a POST request to the server with the file data
-            out.println("POST /example.txt HTTP/1.0");
-            out.println("Content-Length: " + fileData.length);
-            out.println();
-            out.write(new String(fileData));
-            out.flush();
+        // Open a file input stream to read the file
+        String workingDirectory = System.getProperty("user.dir");
+        String saveFilePath = workingDirectory + File.separator + "serverstorage"+File.separator+filename+".txt";
+        FileInputStream fileInputStream = new FileInputStream(saveFilePath);
 
-            // Open an input stream to read the server's response
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // Read the file into a byte array
+        byte[] fileData = new byte[fileInputStream.available()];
+        fileInputStream.read(fileData);
 
-            // Read the server's response
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = in.readLine()) != null) {
-                response.append(line).append("\n");
-            }
+        // Close the file input stream
+        fileInputStream.close();
 
-            // Print the server's response
-            System.out.println(response);
+        // Send a POST request to the server with the file data
+        out.println("POST /example.txt HTTP/1.0");
+        out.println("Content-Length: " + fileData.length);
+        out.println();
+        out.write(new String(fileData));
+        out.flush();
 
+        // Open an input stream to read the server's response
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        // Read the server's response
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line).append("\n");
         }
 
-        // Close the socket
-        socket.close();
+        // Print the server's response
+        System.out.println(response);
+
+    }
+
+    private static void downloadfiles(String filename) throws IOException {
+        String workingDirectory = System.getProperty("user.dir");
+        String saveFilePath = workingDirectory + File.separator + "serverstorage" + File.separator + filename+".txt";
+
+
+
+        // Open an output stream to send the GET request
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Send the GET request
+        out.println("GET /" + saveFilePath + " HTTP/1.0");
+        out.println();
+        out.flush();
+
+        // Open an input stream to read the server's response
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        // Read the server's response
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line).append("\n");
+        }
+
+        // Check if the server's response is a "200 OK"
+        if (response.toString().startsWith("HTTP/1.0 200 OK")) {
+            // Extract the file data from the response
+            int fileStart = response.indexOf("\r\n\r\n") + 4;
+            int fileEnd = response.length() - 1;
+            String fileData = response.substring(fileStart, fileEnd);
+            saveFilePath = workingDirectory + File.separator + "clientstorage" + File.separator + "Received" + System.currentTimeMillis() + "_" + filename + ".txt";
+
+            // Open a file output stream to save the file
+            FileOutputStream fileOutputStream = new FileOutputStream(saveFilePath);
+
+            // Write the file data to the file output stream
+            fileOutputStream.write(fileData.getBytes());
+
+            // Close the file output stream
+            fileOutputStream.close();
+            System.out.println("File saved to disk.");
+        } else {
+            System.out.println("Error: " + response);
+        }
     }
 }
